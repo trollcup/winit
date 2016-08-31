063ee76457f4139eca33fb105b8066a7772b5fd5
Brandon Birschbach <brandon.birschbach@gmail.com>
2016-08-31 17:46:18 -0600

    Added modal window for product page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,17 +22,6 @@ $(document).ready(function() {
 		rewindNav: false,
 	});
 });
-
-// Electronics Owl Carousel 
-$(document).ready(function() {
-	$('#winit-slider-2').owlCarousel({
-		items: 3,
-		stagePadding: 100,
-		navigation: true,
-		pagination: true,
-		rewindNav: false,
-	});
-});
 $(document).ready(function() {
 	$('#winit-slider-2').owlCarousel({
 		navigation: true,
@@ -68,3 +57,85 @@ $(document).ready(function() {
 // $(window).load(function() {
 // 	$('#modal-startup').modal('show');
 // });
+
+
+
+
+// js added by Brandon
+
+// Owl Carousel for the modal window
+    $(document).ready(function() {
+     
+      var sync1 = $("#sync1");
+      var sync2 = $("#sync2");
+     
+      sync1.owlCarousel({
+        singleItem : true,
+        slideSpeed : 1000,
+        navigation: true,
+        pagination:false,
+        afterAction : syncPosition,
+        responsiveRefreshRate : 200,
+        navigationText: ["<",">"],
+      });
+     
+      sync2.owlCarousel({
+        items : 5,
+        itemsDesktop      : [1199,5],
+        itemsDesktopSmall     : [979,5],
+        itemsTablet       : [768,5],
+        itemsMobile       : [479,5],
+        pagination:false,
+        responsiveRefreshRate : 100,
+        afterInit : function(el){
+          el.find(".owl-item").eq(0).addClass("synced");
+        }
+      });
+     
+      function syncPosition(el){
+        var current = this.currentItem;
+        $("#sync2")
+          .find(".owl-item")
+          .removeClass("synced")
+          .eq(current)
+          .addClass("synced")
+        if($("#sync2").data("owlCarousel") !== undefined){
+          center(current)
+        }
+      }
+     
+      $("#sync2").on("click", ".owl-item", function(e){
+        e.preventDefault();
+        var number = $(this).data("owlItem");
+        sync1.trigger("owl.goTo",number);
+      });
+     
+      function center(number){
+        var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+        var num = number;
+        var found = false;
+        for(var i in sync2visible){
+          if(num === sync2visible[i]){
+            var found = true;
+          }
+        }
+     
+        if(found===false){
+          if(num>sync2visible[sync2visible.length-1]){
+            sync2.trigger("owl.goTo", num - sync2visible.length+2)
+          }else{
+            if(num - 1 === -1){
+              num = 0;
+            }
+            sync2.trigger("owl.goTo", num);
+          }
+        } else if(num === sync2visible[sync2visible.length-1]){
+          sync2.trigger("owl.goTo", sync2visible[1])
+        } else if(num === sync2visible[0]){
+          sync2.trigger("owl.goTo", num-1)
+        }
+        
+      }
+     
+    });
+
